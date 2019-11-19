eccf81bb36abdc31d1853ffbf4d25cffab3da1a1
zhenyuli <zhenyu.li@tum.de>
2019-11-19 21:15:48 +0100

    tested init_DRLagent outside of the nrp with python3

--- testbench/init_DRLagent_tb.py
+++ testbench/init_DRLagent_tb.py
@@ -1,20 +1,17 @@
 CONFIGURATION = {}
+#Modified for test outside nrp, still use python2.
+#change log:
+#remove all clientLogger.info() . May use print instead
 
-# internal keras-rl agent to persist
-@nrp.MapVariable("agent", initial_value=None, scope=nrp.GLOBAL)
-@nrp.MapVariable("conf",initial_value=CONFIGURATION)
-@nrp.Robot2Neuron()
 def init_DRLagent(t, agent, conf):
     # initialize the keras-rl agent
     if agent.value is None:
         # import keras-rl in NRP through virtual env
-        clientLogger.info('before import')
-        import site, os
-        #site.addsitedir(os.path.expanduser('~/env_NRP_py3/lib/python3.5/site-packages'))
-        #site.addsitedir(os.path.expanduser('~/workspace/NRP_TF_venv/lib/python2.7/site-packages'))
-        site.addsitedir('/home/zhenyu/workspace/nrp_tf_venv/lib/python2.7/site-packages')
+
+        #import site, os
+        #site.addsitedir('/home/zhenyu/workspace/nrp_tf_venv/lib/python2.7/site-packages')
         from keras.models import Model, Sequential
-        clientLogger.info('first keras model')
+
         from keras.layers import Dense, Activation, Flatten, Input, concatenate
         from keras.optimizers import Adam, RMSprop
         
@@ -23,7 +20,7 @@ def init_DRLagent(t, agent, conf):
         from rl.random import OrnsteinUhlenbeckProcess
 
         from keras import backend as K
-        clientLogger.info('after')
+
         K.clear_session()
 
         actor_layers = conf.value.get('DDPG_Agent',{}).get('ActorNet',{}).get('hidden_layers',[[128,'relu'],[128,'relu'],[128,'relu']])
@@ -41,11 +38,10 @@ def init_DRLagent(t, agent, conf):
         compiler_args = conf.value.get('DDPG_Agent',{}).get('compiler',{'metrics':['mae']})
         optimizer = conf.value.get('DDPG_Agent',{}).get('optimizer','Adam(lr=.001, clipnorm=1.)')
 
-
         nS = 58
         nA = 24
 
-        clientLogger.info('INIT AGENT')
+        print("INIT AGENT")
         
         # create the nets for rl agent
         # actor net s --> a
@@ -64,7 +60,7 @@ def init_DRLagent(t, agent, conf):
         for layer in actor_layers:
             actor.add(Dense(layer[0],activation=layer[1]))
         actor.add(Dense(nA,activation=actor_act))
-
+        actor.summary()
         # critic net (s,a) --> Q(s,a)
         action_input = Input(shape=(nA,), name='action_input')
         observation_input = Input(shape=(1,) + (nS,), name='observation_input')
@@ -85,21 +81,39 @@ def init_DRLagent(t, agent, conf):
         x = Dense(1,activation=critic_act)(x)
 
         critic = Model(inputs=[action_input, observation_input], outputs=x)
-
+        critic.summary()
         
         # instanstiate rl agent
-        agent.value = DDPGAgent(nb_actions=nA, actor=actor, critic=critic, critic_action_input=action_input, memory=memory, nb_steps_warmup_critic=10, nb_steps_warmup_actor=10, random_process=random_process, gamma=.99, batch_size=5, target_model_update=1e-3, delta_clip=1.)
-        agent_args = {}
-        #agent.value = DDPGAgent(nb_actions=nA, actor=actor, critic=critic, critic_action_input=action_input, memory=eval(memory), random_process=eval(random_process), **{'gamma':0.99})
+        #agent.value = DDPGAgent(nb_actions=nA, actor=actor, critic=critic, critic_action_input=action_input, memory=memory, nb_steps_warmup_critic=10, nb_steps_warmup_actor=10, random_process=eval(random_process), gamma=.99, batch_size=5, target_model_update=1e-3, delta_clip=1.)
+
+        #agent_args = {}
+        #memory = SequentialMemory(limit=100000, window_length=1)
+        #random_process = OrnsteinUhlenbeckProcess(theta=.15, mu=0., sigma=.2, size=nA)
+        #agent.value = DDPGAgent(nb_actions=nA, actor=actor, critic=critic, critic_action_input=action_input, memory=memory, random_process=random_process)
+        agent.value = DDPGAgent(nb_actions=nA, actor=actor, critic=critic, critic_action_input=action_input, memory=eval(memory), random_process=eval(random_process), **agent_args)
         agent.value.training = True
         #PATH = '/home/.opt/nrpStorage/template_new_1/ddpg_weights.h5'
         PATH = conf.value.get('DDPG_Agent',{}).get('weights_sav_path',"/home/.opt/nrpStorage/template_new_1/ddpg_weights.h5")
         if os.path.isfile(PATH):
-            clientLogger.info('weights loaded!')
+            print("weights loaded!")
             agent.value.load_weights(PATH)
         #agent.value.compile(Adam(lr=.001, clipnorm=1.), metrics=['mae'])
         agent.value.compile(optimizer=eval(optimizer),**compiler_args)
-        clientLogger.info('***********************')
-        clientLogger.info('DDPG agent ready!')
-        clientLogger.info('***********************')
-        
+        print("Ready")
+
+
+
+if __name__ == '__main__':
+	class nrpVar:
+		def __init__(self):
+			self.value = None
+	import os
+	import yaml
+	CONFIG_PATH = "../simulation_config" #path for all configuration files
+	configurations = os.listdir(CONFIG_PATH)
+	configurations = list(filter(lambda x: x.endswith('yaml'), configurations)) #only .yaml file should be in the list
+	conf = nrpVar()
+	conf.value = yaml.load(open(CONFIG_PATH+'/'+configurations[0]))
+	agent = nrpVar()
+	init_DRLagent(0, agent, conf)
+	print("test pass")
