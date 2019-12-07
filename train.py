fbba6c1db6b76d59b02d63e5c5ffcf8418f3aa47
Zhenyu Li <zhenyu.li@tum.de>
2019-12-07 21:59:57 +0100

    finish new training script

--- train.py
+++ train.py
@@ -3,145 +3,164 @@ import pandas as pd
 import time
 import sys,os
 import yaml
+import argparse
 from multiprocessing import Process
 from hbp_nrp_virtual_coach.virtual_coach import VirtualCoach
 
-EXPERIMENT = "Neurorobotics-Musculoskeletal-Walking-Robot-DRL_0"
-CONFIG_PATH = "./simulation_config" #path for all configuration files
 
-def sim_start(experiment,server,conf):
-#This function will monitor and control the reinforcement learning process, 
-
+def sim_start(experiment,server,config_file_path,results_save_path):
 	sim = vc.launch_experiment(experiment)
+	print "Configuring..."
 
-	config = yaml.load(conf) #load configurations from .yaml file into a dictionary 
+	#load configurations from .yaml file into a python dictionary
+	config = yaml.load(open(config_file_path))
 
 	tf_environment = sim.get_transfer_function('environment').splitlines()
-
 	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!TODO!!!!!!!!!!!!!!!!!!!!!!!
-	tf_ddpg = sim.get_transfer_function('fake_init_DRLagent').splitlines()
+	tf_ddpg = sim.get_transfer_function('fake_init_DRLagent').splitlines()#############
 	#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!TODO!!!!!!!!!!!!!!!!!!!!!!!
 
-	#add the configuration dictionary as a string in the first line of the code.  
+	#Substritue the first line of the transfer function with the configuration dict..
 	tf_environment[0] = "CONFIGURATION = " + str(config)
 	tf_ddpg[0] = "CONFIGURATION = " + str(config)
 
 	"\n".join(tf_environment)
 	"\n".join(tf_ddpg)
 
-	#sim.edit_transfer_function('environment',tf_environment)
-	#sim.edit_transfer_function('init_DRLagent',tf_ddpg)
+	sim.edit_transfer_function('environment',tf_environment)
+	sim.edit_transfer_function('init_DRLagent',tf_ddpg)
+
+	sim.start()
 
 	conf_train = config['Training_Script']
-	MAX_EP = int(conf_train.get("Max_Epoch",100))
-	#sim.start()
+	MAX_EP = int(conf_train.get("Max_Epoch",1000))
+	MAX_ITER = int(conf_train.get("Max_Iteration_per_Epoch",100))
 
-	history = {'height':[],'reward':[],'total_reward':[]} #a very large dict contains all history
-	history_total_reward = {'total_reward':[]} #only contains the total reward for each epoch, much faster to read.
+	#init csv file for the whole training process
+	history = pd.DataFrame(columns=['itr_idx','height','reward'])
 	for ep in range(MAX_EP):
 	#iteration over episodes
+		nFailed = 0
 		while True:
-		#try to launch the simulation. error may occur while restarting, therefore pack this part in a loop with try/except. 
-			try:
-				print "restarting"
-				sim.start() 
-				break
-			except KeyboardInterrupt:
+		#restart the simulation by stop&start, quit if failed for more than 15 times
+			if nFailed>=15:
+				print "Unable to launch the experiment, check the backend."
 				sys.exit()
-			except:
-				print "waiting for restart"
-				time.sleep(2)
+			elif: sim.get_state() == "stopped":
 				try:
-					print "try to launch"
-					sim = vc.launch_experiment(EXPERIMENT)
-				except:
-					print "launch failed, try again"
+					sim = vc.launch_experiment(experiment)
+					sim.start()
+					break
+				except KeyboardInterrupt:
+					sys.exit()
+				except Exception as e:
+					print "Great Job! got an error: ", e
+					nFailed += 1
+					print "number of trial: ", nFailed, ", try it again later."
+					time.sleep(1)
+			else:
+				break
 
 		itr_idx = 0
 		height = 1
 		total_reward = 0
-		while(itr_idx<5 and height>0.5 and height<1.5):
+		while(itr_idx<MAX_ITER and height>0.5 and height<1.5):
 		#early terminate the simulation if it jump too high or fall down. the hight of pelvis is used here.
 		#terminate the simulation when reaches maximum steps.
 
 			#retrive current states from csv file
 			csv_curr_stat = sim.get_last_run_csv_file('curr_stat.csv')
-			print csv_curr_stat
 			lst_line = csv_curr_stat.splitlines()[-1].split(',') #last line of the csv, namely the newest states.
 			if lst_line[0] != "itr_idx": #avoiding the case that the csv file contains only the header.
-				print lst_line
+				#print lst_line
 				itr_idx = int(lst_line[0])
 				height = float(lst_line[1])
-				reward = float(lst_line[2])
-				history['height'].append(height)
-				history['reward'].append(reward)
-				total_reward += reward
-				history['total_reward'].append(total_reward)
-			time.sleep(0.5)
-
-		history_total_reward['total_reward'].append(total_reward)
-		print itr_idx
-		print height
-		
+				#reward = float(lst_line[2])
+			time.sleep(0.25)
 
 		sim.stop()
-		time.sleep(1)
-	sim.stop()
-	pd.DataFrame(history_total_reward).to_csv(CONFIG_PATH+'/results'+conf.split('/')[-1][:-5]+'_total_reward'+'.csv')
-	pd.DataFrame(history).to_csv(CONFIG_PATH+'/results'+conf.split('/')[-1][:-5]+'.csv')
 
-def evaluate():
-	import matplotlib.pyplot as plt 
-	results = os.listdir(CONFIG_PATH+'/results')
+		#transform csv_curr_stat from string to 2D-array
+		data = csv_curr_stat.splitlines()
+		data = list(map(lambda x: x.split(','),data))
+		history = history.append(pd.DataFrame(data=data,columns=['itr_idx','height','reward']))
+
+		while sim.get_state() != "stopped":
+			pass
+
+	#store the history, file name is the same as the config file's name
+	history.to_csv(results_save_path+'/'+conf.split('/')[-1][:-5]+'.csv')
+	import seaborn as sns
+	sns.kdeplot(history.reward)
+
+
+def evaluate(results_save_path):
+	import seaborn as sns
+	results = os.listdir(results_save_path)
 	best_config = None
+	best_data = None
 	largest_reward = -float('Inf')
 	for result in results:
-		if result[-16:-4] == 'total_reward':
-			curr_config = result[:-17]
-			data = pd.read_csv(CONFIG_PATH+'/results'+result)
-			total_reward = data['total_reward'][-1]
-			if total_reward >= largest_reward:
-				best_config = curr_config
-				largest_reward = total_reward
-			plt.plot(data['total_reward'])
-	print "The best configuration is "+best_config
+		curr_config = result[:-4]
+		data = pd.read_csv(results_save_path+'/'+result)
+		avg_reward = data['reward'][-100:].astype(float).mean() #average reward for last 100 steps
+		if total_reward >= largest_reward:
+			best_config = curr_config
+			largest_reward = avg_reward
+			best_data = data
+	sns.kdeplot(best_data.reward)
+	print "the best configuration is: ",best_config
 
 
+def main(experiment,config_path,results_save_path,environment,storage_username,storage_password):
+	vc = VirtualCoach(environment=environment, storage_username=storage_username, storage_password=storage_password)
+	vc.print_cloned_experiments()
 
-
-vc = VirtualCoach(environment='local', storage_username='nrpuser', storage_password='password')
-vc.print_cloned_experiments()
-
-#retrive the list of availiable servers
-servers = vc.print_available_servers()
-if servers == []:
-	print "No available server"
-	sys.exit()
-else:
-	print len(servers)," server(s) available."
-
-
-#list of configuration files 
-configurations = os.listdir(CONFIG_PATH)
-configurations = list(filter(lambda x: x.endswith('yaml'), configurations)) #only .yaml file should be in the list
-
-while True:
-	if len(configurations) > 0:
-	#check whether there is configuration not being simulated yet.
-		if len(servers) > 0:
-		#allocate an availiable server to the simulation, if no server is available, just wait. 
-			curr_path = "./simulation_config/" + configurations.pop()
-			print curr_path
-			conf = open(curr_path)
-			server = servers.pop()
-			#create a new process to run the simulation
-			p = Process(target=sim_start, args=(EXPERIMENT,server,conf))
-			p.start()
-		else:
-			print len(configurations)," configurations in queue. Waiting for available server"
-			while vc.print_available_servers() == []:
-				time.sleep(1)
-			servers = vc.print_available_servers()
+	#retrive the list of availiable servers
+	servers = vc.print_available_servers()
+	if servers == []:
+		print "No available server, exit."
+		sys.exit()
 	else:
-		print "Finished!"
-		break
+		print len(servers)," server(s) available."
+
+	#list of configuration files 
+	configurations = os.listdir(CONFIG_PATH)
+	configurations = list(filter(lambda x: x.endswith('yaml'), configurations)) #only yaml file should be in the list
+
+	while True:
+		if len(configurations) > 0:
+		#check whether there is configuration not being simulated yet.
+			if len(servers) > 0:
+			#allocate an availiable server to the simulation, if no server is available, just wait. 
+				curr_config_path = config_path + "/" + configurations.pop()
+				print curr_config_path
+
+				server = servers.pop()
+
+				#create a new process to run the simulation
+				p = Process(target=sim_start, args=(experiment,server,curr_config_path,results_save_path))
+				p.start()
+			else:
+				print len(configurations)," configurations in queue. Waiting for available server"
+				while vc.print_available_servers() == []:
+					time.sleep(1)
+				servers = vc.print_available_servers()
+	print "Training Complete!"
+	evaluate(results_save_path)
+
+
+#----------------------------------------------------------------------#
+parser = argparse.ArgumentParser(description='Parallel training script.')
+parser.add_argument('--experiment','-x',help='name of the experiment to be launched. default: Neurorobotics-Musculoskeletal-Walking-Robot-DRL_0', default='Neurorobotics-Musculoskeletal-Walking-Robot-DRL_0')
+parser.add_argument('--config_path','-c',help='path to configuration files. default: ./simulation_config',default='./simulation_config')
+parser.add_argument('--results_path','-r',help='path to simulatuion results. frfault: ./results', default='./results')
+parser.add_argument('--environment','-e',help='environment of NRP backend. default: local', default='local')
+parser.add_argument('--storage_username','-n',help='NRP storage user name. default: nrpuser', default='nrpuser')
+parser.add_argument('--storage_password','-p',help='NRP storage password. default: password', default='password')
+args = parser.parse_args()
+
+if __name__ == "__main__":
+	if args.config_path[-1] == '/':
+		args.config_path = args.config_path[:,-1]
+	main(args.experiment,args.config_path,args.results_path,args.environment,args.storage_username,args.storage_password)
